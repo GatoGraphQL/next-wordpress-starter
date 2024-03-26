22640eff54966f7dd86e4951449c1cd88fef8a03
leoloso <leo@getpop.org>
2024-03-26 15:36:56 +0800

    format

--- src/lib/provider.js
+++ src/lib/provider.js
@@ -6,6 +6,7 @@ export function getProvider() {
   const providers = ['wpgraphql', 'gatographql'];
   const defaultProvider = 'wpgraphql';
   const selectedProvider = (process.env.WORDPRESS_GRAPHQL_PROVIDER || '').toLowerCase();
+
   if (!selectedProvider || !providers.includes(selectedProvider)) return defaultProvider;
 
   return selectedProvider;
