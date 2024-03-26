b7f39f2b6278460b8512a44149f5847d2d7c81f3
leoloso <leo@getpop.org>
2024-03-26 15:29:35 +0800

    not async

--- src/lib/provider.js
+++ src/lib/provider.js
@@ -2,7 +2,7 @@
  * getProvider
  */
 
-export async function getProvider() {
+export function getProvider() {
   const providers = ['wpgraphql', 'gatographql'];
   const defaultProvider = 'wpgraphql';
   const selectedProvider = (process.env.WORDPRESS_GRAPHQL_PROVIDER || '').toLowerCase();
